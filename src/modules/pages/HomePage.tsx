bbc5a973e9bf01f10c944605f2b4abbdc92b2e9e
Hoang Giang <truonggiang1139@gmail.com>
2023-04-28 09:43:42 +0700

    add route

--- src/modules/pages/HomePage.tsx
+++ src/modules/pages/HomePage.tsx
@@ -1,9 +1,14 @@
+import { Link } from "react-router-dom";
+
 interface Props {}
 
 const HomePage = (props: Props) => {
   return (
     <div>
       <h2>Home Page</h2>
+      <Link to={"/product"} className="text-blue-600 ">
+        Product
+      </Link>
     </div>
   );
 };
